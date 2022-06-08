40343490613f630d2cdda4597560cc1e6393ab7c
Zohid Hayitov <102049840+zohidhayitov@users.noreply.github.com>
2022-06-09 04:47:25 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#Salom hammaga. Bugun birinchi dars
+#Mullif: Zohid Hayitov
+print("Salom")
+print('Salom')
+print(2+3*3)#Bu amal qo'shish va ko'paytirishdan iborat
+# print("Assalomu alaykum")
+print("Hayrli tong") #Bunda konsolga hayrli tong deb chiqadi
+print("""Bugun qanday  
+kunligini bilasizmi?""")
+print('''Toshkent chiroyli shahar. 
+Siz toshkentga kelganmisiz''')
+print("Men Qashqadaryolikman.\nMeni vatanim Qashqadaryo")
+print(7+8)
+print(9-8)
+print(17-8)
+print(9/3)
+print(30/4)
+print(5*8)
+print(2**5)
+print(3**2)
+print(17//3)
+print(18//4)
+print(17/3)
+print(17//3)
+print("To'rtning kubi", 4**3, "ga teng")
+print("G'ozlar galasi uchib keldi")
+print("Bugun \"Bravo\" jamoasi konsert beradi")
+print('Men "Acer" markali kompyuter sotib oldim')
